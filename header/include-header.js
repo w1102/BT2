ee95d45f159290140147952f07156362b323b839
w1102 <tranwang1102@gmail.com>
2021-11-11 20:20:25 +0700

    header: test jquery animation

--- header/include-header.js
+++ header/include-header.js
@@ -10,11 +10,27 @@ $(document).ready(() => {
 			case 'chart': $('#chart').addClass("active"); break;
 		}
 		
+		$('#root').innerHeight( window.innerHeight - $('#header').innerHeight()).toggle('fast')
+		
 		
 		setTimeout(() => {
-			$('#root').innerHeight( window.innerHeight - $('#header').innerHeight()).css('visibility', 'visible')
+			
+			//$('#root').innerHeight( window.innerHeight - $('#header').innerHeight()).toggle('fast')
+					
+			
+			//$('#root').innerHeight( window.innerHeight - $('#header').innerHeight()).css('visibility', 'visible')
+			
+			
 			setTimeout(() => {
-				$('#root').innerHeight( window.innerHeight - $('#header').innerHeight())
+				
+				//$('#root').innerHeight( window.innerHeight - $('#header').innerHeight())
+				
+				
+	
+				
+				//$('#root').toggle('slow')
+				
+				
 			}, 800)
 		}, 500)
 		
