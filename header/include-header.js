0ac6a3c06915c9d6e634fabbb03780642865c996
w1102 <tranwang1102@gmail.com>
2021-11-11 11:25:14 +0700

    first commit

---
+++ header/include-header.js
@@ -0,0 +1,22 @@
+import * as JQuery from '/node_modules/jquery/dist/jquery.min.js'
+
+$(document).ready(() => {
+	
+	
+	$('#header').load('/header/header.html', (response, status, xhr) => {
+				
+		switch ( $('#header').attr("data-active") ) {
+			case 'calc' : $('#calc').addClass("active"); break;
+			case 'chart': $('#chart').addClass("active"); break;
+		}
+		
+		
+		setTimeout(() => {
+			$('#root').innerHeight( window.innerHeight - $('#header').innerHeight()).css('visibility', 'visible')
+			setTimeout(() => {
+				$('#root').innerHeight( window.innerHeight - $('#header').innerHeight())
+			}, 800)
+		}, 500)
+		
+	})
+})
